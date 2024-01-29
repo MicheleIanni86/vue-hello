f5e17c3f218913f27b0ebf416079c69daf5dce05
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-29 15:05:00 +0100

    feat: Printed on The Screen a Message Inside an h1, Using the Data.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,25 @@ const { createApp } = Vue;
 
 const app = createApp({
     data() {
-
-    },
+        return {
+            title: 'MESSAGGIO GENERATO!',
+        };
+    }
 }).mount('#root');
 // END SCRIPT VUEJS-------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+/*Descrizione:
+Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un'immagine, presa anch'essa da un data.*/
